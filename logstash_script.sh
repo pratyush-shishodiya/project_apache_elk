d464ad4d5344a8125b89ca4f851201971b42851b
Pratyush <Pratyushshishodiya@gmail.com>
2022-10-27 03:29:37 +0530

    update in pipeline

---
+++ logstash_script.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+
+cd /Users/pratyushshishodiya/Desktop/elasticsearch-8.3.3
+bin/elasticsearch
+
+
+cd /Users/pratyushshishodiya/Desktop/logstash-8.3.3
+bin/kibana
+
+
+cd /Users/pratyushshishodiya/Desktop/Project_apache
